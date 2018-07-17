c42005683c8853d4b6c853f8395417c78a81f998
XUHO3 <howells.xu@oocl.com>
2018-07-17 19:08:01 +0800

    first start

--- js/todo.js
+++ js/todo.js
@@ -22,4 +22,75 @@ $(document)
         }
 
         // code to be implemented
+
+
+        $("#button").click(function () {
+            if ($(".input-text").text() != null) {
+                let uuid = generateUUID();
+                let input = $(".input-text").val();
+                $("ol").append(`<li id=${uuid} class=\"\">
+                      <input name=\"done-todo\" type=\"checkbox\" class=\"checked\" >${input} </li>`);
+                $(".input-text").val("");
+            }
+            else {
+                alert("should not null")
+            }
+        })
+
+        $(".done-todo").change(function () {
+            if ($(this).parent().attr("class") == "") {
+                $(this).parent().attr("class", "checked");
+            }
+            else {
+                $(this).parent().attr("class", "");
+            }
+        })
+
+
+        $("span").click(function () {
+              $(this).val("");
+              $(this).parent().d
+        })
+
+
+        $("a").click(function () {
+
+            if ($(this).attr("data-filter") === "all") {
+                // $("ol").html("");
+                $("ol").children().each(function () {
+                    $(this).css("display",'block');
+                })
+            }
+            if ($(this).attr("data-filter") === "active") {
+                $("ol").children().each(function () {
+                   if($(this).attr("class")==="checked"){
+                       $(this).css("display",'none');
+                   }
+                   else {
+                       $(this).css("display",'block');
+                   }
+                })
+            }
+            if ($(this).attr("data-filter")=== "complete") {
+                $("ol").children().each(function () {
+                    if($(this).attr("class")!="checked"){
+                        $(this).css("display",'none');
+                    }
+                    else{
+                        $(this).css("display",'block');
+                    }
+                })
+            }
+        })
+
+
+        $("ol >li").dblclick(function () {
+            $(this).attr("contenteditable","true");
+            $(this).bind('keypress',function(event) {
+                if (event.keyCode == "13") {
+                    $(this).attr("contenteditable", "false");
+                }
+            });
+        })
+
     });
